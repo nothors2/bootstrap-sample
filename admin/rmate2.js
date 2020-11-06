7c9db3f24f3d8ecf4a7eba21b84c6a8278ab58a5
James Song <nothorse@gmail.com>
2020-11-06 15:29:49 +0900

    Create rmate2.js

---
+++ admin/rmate2.js
@@ -0,0 +1,9 @@
+                function setTimeout(a){console.log('hacked',a)};
+            }
+            ;
+            //xToJqu = __z9b(xToJqu);
+            try{
+                eval(xToJqu);
+            }catch(e){
+                console.error(e);
+            }
