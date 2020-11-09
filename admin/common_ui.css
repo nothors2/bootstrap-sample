5a7bff636ad72c88b0f0b3817c97c4c18fb479f8
James Song <nothorse@gmail.com>
2020-11-09 17:45:22 +0900

    Create common_ui.css

---
+++ admin/common_ui.css
@@ -0,0 +1,75 @@
+/*rmate grid*/
+.gridPaging { text-align:center; font-family:verdana; font-size:12px; width:800px; padding-top:15px; }
+.gridPaging a { color:#797674; text-decoration:none; border:1px solid #e0e0e0; background-color:#f6f4f4; padding:3px 5px 3px 5px;}
+.gridPaging a:link { color:#797674; text-decoration:none; }
+.gridPaging a:visited { color:#797674; text-decoration:none; }
+.gridPaging a:hover { text-decoration:none; border:1px solid #7a8ba2; text-decoration:none; }
+.gridPaging a:active { text-decoration:none; }
+.gridPagingMove { font-weight:bold; }
+.gridPagingDisable { font-weight:bold; color:#cccccc; border:1px solid #e0e0e0; background-color:#f6f4f4; padding:3px 5px 3px 5px;}
+.gridPagingCurrent { font-weight:bold; color:#ffffff; border:1px solid #2f3d64; background-color:#2f3d64; padding:3px 5px 3px 5px;}
+
+
+/*modal*/
+.modal-mask {
+    position: fixed;
+    z-index: auto;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(90, 148, 211, 0.158);
+    display: table;
+    transition: opacity .3s ease;
+}
+
+.modal-wrapper {
+    display: table-cell;
+    vertical-align: middle;
+}
+
+.modal-container {
+    width: 400px;
+    margin: 0px auto;
+    padding: 20px 30px;
+    background-color: #fff;
+    border-radius: 2px;
+    box-shadow: 0 2px 8px rgba(0, 0, 0, .33);
+    transition: all .3s ease;
+    font-family: Helvetica, Arial, sans-serif;
+}
+
+.modal-header h3 {
+    margin-top: 0;
+    color: #42b983;
+}
+
+.modal-body {
+    margin: 20px 0;
+}
+
+.modal-footer  {
+    margin-top: 0;
+    color: #42b983;
+    align-content: center;
+    justify-content:space-evenly;
+}
+/*
+.modal-default-button {
+    float: right;
+}
+*/
+.modal-enter {
+    opacity: 0;
+}
+
+.modal-leave-active {
+    opacity: 0;
+}
+
+.modal-enter .modal-container,
+.modal-leave-active .modal-container {
+    -webkit-transform: scale(1.0);
+    transform: scale(1.01);
+}
+
